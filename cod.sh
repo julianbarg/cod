f1e3bc157c6cf695cd971a238cec4afd0ccac632
Julian Barg <barg.julian@gmail.com>
2022-03-06 11:53:02 -0500

    Implemented most important functions.

--- cod.sh
+++ cod.sh
@@ -43,7 +43,7 @@ function filter_folder {
 		echo "No filter provided."
 		exit 1
 	fi 
-	
+
 	if [ ! -z $ITERATION ]; then
 		to_code="$( filter_timestamp $ITERATION $to_code )"
 	fi
@@ -77,8 +77,8 @@ function filter_timestamp {
 #
 # }
 
-# #TODO: code document--show document and insert codes.
-# function code_document {
+# #TODO: code document--show document(s) and insert codes.
+# function code {
 # 	FILE=$1
 # 	CODES=$2
 # 	ITERATION=$3
@@ -89,9 +89,106 @@ function filter_timestamp {
 # #TODO: replace code $CODE in document(s) with code $NEW_CODE.
 # # Or if $NEW_CODE is not provided, show documents and provide new
 # # codes for each and replace the old one with new one.
+# # Just needs to remove string and call insert_code.
 # function recode {
 #
 # }
+
+# Create the section with codes for doc if it does not exist.
+function mk_coding {
+	FILE=$1
+	if [[ $(grep -L -E "^## Codes$" $FILE) ]]; then
+		echo -e "\n\n## Codes\n\n* @iteration\n" >> $FILE
+	fi
+	return
+}
+
+# Test if document already contains code and exit if exists twice.
+function code_absent {
+	CODE=$1
+	FILE=$2
+
+	case `grep "^\* #${CODE}$" ${FILE} >/dev/null; echo $?` in
+		0)
+		  return 1
+		  ;;
+		1)
+		  return 0
+		  ;;
+		*)
+		  echo "Error: erroneous code search."
+		  ;;
+	esac
+}
+
+function iteration_absent {
+	ITERATION=$1
+	FILE=$2
+	case `grep "^\* @iteration.*${ITERATION}" ${FILE} >/dev/null; echo $?` in
+		0)
+		  return 1
+		  ;;
+		1)
+		  return 0
+		  ;;
+		*)
+		  echo "Error: erroneous iteration search."
+		  ;;
+	esac
+}
+
+# Utility function that handles insertion of code.
+function insert_code_ {
+	CODE=$1
+	FILE=$2
+	if code_absent ${CODE} ${FILE}; then
+	 		echo "* #$CODE" >> FILE
+	fi
+}
+
+function insert_iteration_ {
+	ITERATION=$1
+	FILE=$2
+	if iteration_absent ${ITERATION} ${FILE}; then
+	 	sed -i -E "s/^\* @iteration.*/& $ITERATION/" $FILE
+	fi
+}
+
+# Insert a specific code and timestamp into the specified document(s).
+function insert_code {
+	POSITIONAL_ARGS=()
+	while [[ $# -gt 0 ]]; do
+		case $1 in 
+			-i|-I|--iteration)
+			  ITERATION="$2"
+			  shift
+			  shift
+			  ;;
+			-*|--*)
+			  echo "Unknown option $1."
+			  return 1
+			  ;;
+			*)
+			  POSITIONAL_ARGS+=("$1")
+			  shift
+			  ;;
+		esac
+	done
+	set -- "${POSITIONAL_ARGS[@]}"\
+	CODE=$1
+	shift
+
+	for i ; do
+		# Some checks here are redundant, like checking for timestamp
+		# if coding section is just inserted by mk_coding, but whatever.
+		# Could eventually figure out how to use mk_coding return 
+		# statement.
+	 	mk_coding $i
+	 	insert_code_ $CODE $i
+	 	insert_iteration_ $CODE $i
+	done
+	return
+}
 
 # #TODO: Open documents one by one, show, and insert code based on
 # #YAML-provided list of codes. May also allow non-YAML provided codes?
